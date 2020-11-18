1723b730561346e1d0dd63382d77abcec6dcb250
DESKTOP-F9CUFM9\user <freshkimdh@gmail.com>
2020-11-18 21:39:21 +0900

    Tensorflow

--- Python Basic/test.py
+++ Python Basic/test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 list_data = [1,2,3]
 array = np.array(list_data)
@@ -7,3 +8,7 @@ print(array.size)
 print(array.dtype)
 
 print("test")
+
+hello = tf.constant("hello world!")
+sess = tf.Session()
+print(sess.run(hello))
