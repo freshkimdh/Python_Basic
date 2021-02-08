8f212e17c263fe90d144b398a1ac49c35860266e
freshkimdh <freshkimdh@gmail.com>
2021-02-08 17:30:23 +0900

    Gradient Descent

--- Python_Grammer/Python_pandas_basic/pandas_1_basic.py
+++ Python_Grammer/Python_pandas_basic/pandas_1_basic.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import os
 
-# series 데이터 
-# series 는 값과 index로 이루어져 있음
-array = pd.Series(['사과','바나나','당근'], index=['a','b','c'])
+print(os.getcwd())
 
-print(array)
-print(array['a']) # 특정 index의 값
+
+# # series 데이터 
+# # series 는 값과 index로 이루어져 있음
+# array = pd.Series(['사과','바나나','당근'], index=['a','b','c'])
+
+# print(array)
+# print(array['a']) # 특정 index의 값
